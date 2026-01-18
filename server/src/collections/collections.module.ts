3f97b6441766c10ed2f17714a1f76fa2247e6421
sree kanth <sreekanth04052005@gmail.com>
2026-01-18 15:20:01 +0530

    fix: Annotation fix for prisma import

--- server/src/collections/collections.module.ts
+++ server/src/collections/collections.module.ts
@@ -3,7 +3,7 @@ import { CollectionsController } from './collections.controller';
 import { CollectionsService } from './collections.service';
 import { FieldValidationService } from './field-validation.service';
 import { RecordsModule } from './records/records.module';
-import { PrismaModule } from '@/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, RecordsModule],
--- server/src/collections/records/records.module.ts
+++ server/src/collections/records/records.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RecordsService } from './records.service';
 import { RecordsController } from './records.controller';
-import { PrismaModule } from '@/prisma/prisma.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
